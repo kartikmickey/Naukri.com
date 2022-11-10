61a51daf29904f7f27198b080ee893fda8636bea
prachi913 <prachitiwari0911@gmail.com>
2022-11-10 08:59:32 +0530

    navbar day-2

---
+++ navbar/navbar.js
@@ -0,0 +1,60 @@
+   
+
+
+   let  navbar=document.querySelector("#navbar")
+   window.onscroll=function(){
+   navbar.classList.add("sticky")
+   }
+   let inside=document.querySelector(".inside");
+   let jobs=document.querySelector(".jobs");
+   jobs.onclick=function(){
+      myfirstclick()
+
+   }
+   jobs.onmouseleave=function(){
+     
+      inside.setAttribute("display","none")
+      // inside.innerHTML=null;
+      // myfirstclick()
+   }
+   // inside.innerHTML=null;
+   // jobs.onclick=function(){
+   //    myfirstclick()
+   // }
+   function myfirstclick(){
+      
+   inside.classList.toggle("hideandsick");
+   console.log("jjj")
+   }
+
+ let insidee=document.querySelector(".insidee");
+ let companies=document.querySelector(".companies")
+ companies.onclick=function(){
+   myfirstclick2()
+ }
+   function myfirstclick2(){
+      
+      insidee.classList.toggle("hideandsick2");
+      console.log("jjj")
+      }
+
+      
+      let serinside=document.querySelector(".serinside")
+      let services=document.querySelector(".services")
+      services.onclick=function(){
+         myfirstclick3()
+      }
+      function myfirstclick3(){
+         serinside.classList.toggle("hideandsick3");
+         console.log("ooo")
+      }
+
+      let eminside=document.querySelector(".eminside")
+      let emp=document.querySelector(".emp")
+      emp.onclick=function(){
+         myfirstclick4()
+      }
+      function myfirstclick4(){
+         eminside.classList.toggle("hideandsick4");
+         console.log("ooo")
+      }
