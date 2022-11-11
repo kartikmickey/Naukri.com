3ad2920dd086623ceebd6305c153c41f94b44561
kartikmickey <741kartikjain@gmail.com>
2022-11-12 00:41:21 +0530

    grow

--- home/home.js
+++ home/home.js
@@ -129,3 +129,47 @@ function plusSlides2(n) {
     slides2[slideIndex2-1].style.display = "grid";
     dots[slideIndex2-1].className += " active";
   }
+
+
+
+  let prev3 = document.querySelector(".prev3")
+prev3.onclick = ()=>{
+    plusSlides3(-1);
+    console.log("hello")
+}
+let next3 = document.querySelector(".next3");
+next3.onclick = ()=>{
+    plusSlides3(1);
+    console.log("hello")
+}
+
+let slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+function plusSlides3(n) {
+    showSlides3(slideIndex3 += n);
+  };
+  function currentSlide3(n) {
+    showSlides3(slideIndex3 = n);
+  }
+
+  function showSlides3(n) {
+    let i;
+    let slides3 = document.getElementsByClassName("mySlides3");
+    next3.style.display = "block";
+    prev3.style.display = "block"
+    if (n == slides3.length) {
+        // slideIndex = 1
+        next3.style.display = "none";
+    }
+    if (n == 1) {
+        // slideIndex = slides.length;
+        prev3.style.display = "none";
+    }
+    for (i = 0; i < slides3.length; i++) {
+      slides3[i].style.display = "none";
+    }
+    slides3[slideIndex3-1].style.display = "block";
+  }
+
+
